b9d35cd4880ccd07f4381e64303018a1bcbe4cba
gayatri <gayatrin13@gmail.com>
2023-01-10 18:32:14 -0800

    updated with thread specific driver

--- src/test/java/com/grid/docker/BrowserTestUsingGridDocker.java
+++ src/test/java/com/grid/docker/BrowserTestUsingGridDocker.java
@@ -1,15 +1,22 @@
 package com.grid.docker;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BrowserTestUsingGridDocker {
 
-	WebDriver driver;
+//	WebDriver driver;
+	ThreadLocal<RemoteWebDriver> drTL = new ThreadLocal<RemoteWebDriver>();
+	RemoteWebDriver driver;
 
 	/*
 	 * start docker grid thru cmd > D: > docker compose -f docker-compose-grid.yml
@@ -18,44 +25,62 @@ public class BrowserTestUsingGridDocker {
 	 * use http://localhost:4444 > sessions : use "secret" pwd to check the running
 	 * application
 	 */
-	@AfterSuite
+	@BeforeSuite
 	public void setUpGrid() {
+//currently this is being done in jenkins ..ie. starting the docker grid and stopping it
+	}
 
+	public WebDriver getDriver() {
+		return drTL.get();
+	}
+
+	public void setDriver(RemoteWebDriver dr) {
+		drTL.set(dr);
 	}
 
 	@Test
-	void remoteChromeDriverTest() throws InterruptedException {
-		driver = WebDriverManager.chromedriver().remoteAddress("http://localhost:4444").create();
-		driver.get("https://practice-site.devops-automation.net");
+	void remoteChromeDriverTest() throws InterruptedException, MalformedURLException {
+//		driver = WebDriverManager.chromedriver().remoteAddress("http://localhost:4444").create();
+		ChromeOptions opt = new ChromeOptions();
+//		opt.setPlatformName("windows11");
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), opt);
+		drTL.set(driver);
+		getDriver().get("https://www.myntra.com");
 		String pageTitle = driver.getTitle();
 		System.out.println("grid chrome .........." + pageTitle);
-		Thread.sleep(3000);
+//		Thread.sleep(6000);
 //		assertTrue(pageTitle.contains("Home"));
 	}
 
 	@Test
-	void remoteEdgeDriverTest() throws InterruptedException {
-		driver = WebDriverManager.edgedriver().remoteAddress("http://localhost:4444").create();
-		driver.get("https://flipkart.com");
+	void remoteEdgeDriverTest() throws InterruptedException, MalformedURLException {
+//		driver = WebDriverManager.edgedriver().remoteAddress("http://localhost:4444").create();
+		EdgeOptions opt = new EdgeOptions();
+//		opt.setPlatformName("windows11");
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), opt);
+		drTL.set(driver);
+		getDriver().get("https://flipkart.com");
 //		String pageTitle = driver.getTitle();
 		System.out.println("grid edge..........");
-		Thread.sleep(3000);
+//		Thread.sleep(6000);
 //		assertTrue(pageTitle.contains("Home"));
 	}
 
 	@Test
-	void remoteFirefoxDriverTest() throws InterruptedException {
-		driver = WebDriverManager.firefoxdriver().remoteAddress("http://localhost:4444").create();
-
-		driver.get("https://amazon.com");
+	void remoteFirefoxDriverTest() throws InterruptedException, MalformedURLException {
+//		driver = WebDriverManager.firefoxdriver().remoteAddress("http://localhost:4444").create();
+		FirefoxOptions opt = new FirefoxOptions();
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), opt);
+		drTL.set(driver);
+		getDriver().get("https://amazon.com");
 //		String pageTitle = driver.getTitle();
 		System.out.println("grid firefox..........");
-		Thread.sleep(3000);
+//		Thread.sleep(6000);
 //		assertTrue(pageTitle.contains("Home"));
 	}
 
-	@BeforeSuite
+	@AfterSuite
 	public void tearDownGrid() {
-
+		getDriver().close();
 	}
 }
